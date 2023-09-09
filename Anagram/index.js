5d47ee0c6e92fc13d17990364f89f15bd2f0b688
Shinyn <mattias.alm@hotmail.com>
2023-09-09 17:12:03 +0200

    Refactored index.js code to work with tests

--- Anagram/index.js
+++ Anagram/index.js
@@ -1,19 +1,31 @@
-const button = document.querySelector('#checkAnagramButton');
-const answer = document.querySelector('#answer');
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('#checkAnagramButton');
 
-button.addEventListener('click', () => {
-  const wordOne = document.querySelector('#wordOne').value;
-  const wordTwo = document.querySelector('#wordTwo').value;
+  button.addEventListener('click', () => {
+    const wordOne = document.querySelector('#wordOne').value;
+    const wordTwo = document.querySelector('#wordTwo').value;
 
+    updateAnswer(areAnagrams(wordOne, wordTwo));
+  });
+});
+
+function updateAnswer(result) {
+  const answer = document.querySelector('#answer');
+  answer.textContent = result;
+}
+
+function areAnagrams(wordOne, wordTwo) {
   if (wordOne.length !== wordTwo.length) {
-    answer.textContent = `'${wordOne}' and '${wordTwo}' are NOT anagrams!`;
+    return `'${wordOne}' and '${wordTwo}' are NOT anagrams!`;
   } else {
     const sortedWordOne = wordOne.toLowerCase().split('').sort().join('');
     const sortedWordTwo = wordTwo.toLowerCase().split('').sort().join('');
     if (sortedWordOne === sortedWordTwo) {
-      answer.textContent = `'${wordOne}' and '${wordTwo}' are anagrams!`;
+      return `'${wordOne}' and '${wordTwo}' are anagrams!`;
     } else {
-      answer.textContent = `'${wordOne}' and '${wordTwo}' are NOT anagrams!`;
+      return `'${wordOne}' and '${wordTwo}' are NOT anagrams!`;
     }
   }
-});
+}
+
+module.exports = { areAnagrams };
